d9119d31c83980a9019e6784cd09fdb48fab2e2a
SimonZaloudek <simon.22.zaloudek@gmail.com>
2022-03-11 16:57:39 +0100

    Init

---
+++ TankyV2/src/Core/Main.java
@@ -0,0 +1,17 @@
+package Core;
+
+/**
+ * 3/15/2022 - 9:17 AM
+ *
+ * @author simon
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        new Main();
+    }
+
+    public Main() {
+
+    }
+}
