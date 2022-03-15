f81d28ac09690fb393842862ff6b73eda4ec8b34
SimonZaloudek <simon.22.zaloudek@gmail.com>
2022-03-15 09:18:55 +0100

    Init for Sem2 project

---
+++ TankyV2/src/sk/uniza/fri/Main.java
@@ -0,0 +1,14 @@
+package sk.uniza.fri;
+/**
+ * Created by IntelliJ IDEA.
+ * User: simon
+ * Date: 3/15/2022
+ * Time: 9:17 AM
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        //TODO Write your code
+        System.out.println("Hello world");
+    }
+}
